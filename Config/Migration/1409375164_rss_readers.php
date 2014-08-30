e4c115dc9aa1788d58bc437a2997a4d790edaf56
MIYAGAWA Takako <nekoget@gmail.com>
2014-08-30 06:05:07 +0000

    added schema and migration file

---
+++ Config/Migration/1409375164_rss_readers.php
@@ -0,0 +1,71 @@
+<?php
+class CreateRssReadersTable extends CakeMigration {
+
+/**
+ * Migration description
+ *
+ * @var string
+ */
+	public $description = '';
+
+/**
+ * Actions to be performed
+ *
+ * @var array $migration
+ */
+	public $migration = array(
+		'up' => array(
+			'create_table' => array(
+				'blocks_rss_readers' => array(
+					'id' => array('type' => 'integer', 'null' => true, 'default' => null, 'key' => 'primary'),
+					'block_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'comment' => 'blocksãƒ†ãƒ¼ãƒ–ãƒ«ã®fk'),
+					'created_user_id' => array('type' => 'integer', 'null' => true, 'default' => null),
+					'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
+					'modified_user_id' => array('type' => 'integer', 'null' => true, 'default' => null),
+					'modified' => array('type' => 'datetime', 'null' => true, 'default' => null),
+					'indexes' => array(
+					),
+					'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB'),
+				),
+				'rss_readers' => array(
+					'id' => array('type' => 'integer', 'null' => true, 'default' => null, 'key' => 'primary'),
+					'blocks_rss_reader_id' => array('type' => 'integer', 'null' => true, 'default' => null),
+					'url' => array('type' => 'text', 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'comment' => 'RSSå–å¾—URL', 'charset' => 'utf8'),
+					'status' => array('type' => 'integer', 'null' => true, 'default' => null, 'length' => 1, 'comment' => 'çŠ¶æ…‹'),
+					'rss' => array('type' => 'text', 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'comment' => 'RSSæœ¬æ–‡', 'charset' => 'utf8'),
+					'created_user_id' => array('type' => 'integer', 'null' => true, 'default' => null),
+					'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
+					'modified_user_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'comment' => 'æœªãƒ­ã‚°ã‚¤ãƒ³ã®äºº:0 ãƒ­ã‚°ã‚¤ãƒ³ã—ã¦ã‚‹äºº:users.id'),
+					'modified' => array('type' => 'datetime', 'null' => true, 'default' => null),
+					'indexes' => array(),
+					'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
+				)
+			),
+		),
+		'down' => array(
+			'drop_table' => array(
+				'rss_readers', 'blocks_rss_readers'
+			),
+		),
+	);
+
+/**
+ * Before migration callback
+ *
+ * @param string $direction up or down direction of migration process
+ * @return boolean Should process continue
+ */
+	public function before($direction) {
+		return true;
+	}
+
+/**
+ * After migration callback
+ *
+ * @param string $direction up or down direction of migration process
+ * @return boolean Should process continue
+ */
+	public function after($direction) {
+		return true;
+	}
+}
--- Config/Schema/schema.php
+++ Config/Schema/schema.php
@@ -1,138 +1,60 @@
 <?php
-/**
- * Schema file
- *
- * @author Noriko Arai <arai@nii.ac.jp>
- * @author Kosuke Miura <k_miura@zenk.co.jp>
- * @link http://www.netcommons.org NetCommons Project
- * @license http://www.netcommons.org/license.txt NetCommons License
- * @copyright Copyright 2014, NetCommons Project
- * @package app.Plugin.RssReaders.Config.Schema
- */
+class RssReaderSchema extends CakeSchema {
 
-/**
- * RssReadersSchema CakeSchema
- *
- * @author Kosuke Miura <k_miura@zenk.co.jp>
- * @package app.Plugin.RssReaders.Config.Schema
- * @SuppressWarnings(PHPMD.LongVariable)
- */
-class RssReadersSchema extends CakeSchema {
-
-/**
- * Database connection
- *
- * @author Kosuke Miura <k_miura@zenk.co.jp>
- * @var string
- */
 	public $connection = 'master';
 
 /**
- * before
+ * Before migration callback
  *
- * @param array $event savent
- * @author Kosuke Miura <k_miura@zenk.co.jp>
- * @return bool
+ * @return boolean Should process continue
  */
-	public function before($event = array()) {
+	public function before() {
 		return true;
 	}
 
 /**
- * after
+ * After migration callback
  *
- * @param array $event event
- * @author Kosuke Miura <k_miura@zenk.co.jp>
- * @return void
+ * @return boolean Should process continue
  */
-	public function after($event = array()) {
+	public function after() {
 	}
 
 /**
- * rss_readers table
  *
- * @author Kosuke Miura <k_miura@zenk.co.jp>
- * @var array
+ * @var array $blocks_rss_readers
+ */
+	public $blocks_rss_readers = array(
+		'id' => array('type' => 'integer', 'null' => true, 'default' => null, 'key' => 'primary'),
+		'block_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'comment' => 'blocksãƒ†ãƒ¼ãƒ–ãƒ«ã®fk'),
+		'created_user_id' => array('type' => 'integer', 'null' => true, 'default' => null),
+		'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'modified_user_id' => array('type' => 'integer', 'null' => true, 'default' => null),
+		'modified' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'indexes' => array(
+
+		),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
+	);
+
+/**
+ *
+ * @var array $rss_readers
  */
 	public $rss_readers = array(
-		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
-		'rss_readers_block_id' => array('type' => 'integer', 'null' => false, 'default' => null),
-		'status' => array('type' => 'integer', 'null' => false, 'default' => '0', 'length' => 3),
-		'language_id' => array('type' => 'integer', 'null' => false, 'default' => '2'),
-		'is_auto_translation' => array('type' => 'boolean', 'null' => false, 'default' => false, 'length' => 1),
-		'translation_engine' => array('type' => 'string', 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-		'title' => array('type' => 'string', 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-		'content' => array('type' => 'text', 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-		'created_user' => array('type' => 'integer', 'null' => true, 'default' => null),
+		'id' => array('type' => 'integer', 'null' => true, 'default' => null, 'key' => 'primary'),
+		'blocks_rss_reader_id' => array('type' => 'integer', 'null' => true, 'default' => null),
+		'url' => array('type' => 'text', 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'comment' => 'RSSå–å¾—URL', 'charset' => 'utf8'),
+		'status' => array('type' => 'integer', 'null' => true, 'default' => null, 'length' => 1, 'comment' => 'çŠ¶æ…‹'),
+		'rss' => array('type' => 'text', 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'comment' => 'RSSæœ¬æ–‡', 'charset' => 'utf8'),
+		'created_user_id' => array('type' => 'integer', 'null' => true, 'default' => null),
 		'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
-		'modified_user' => array('type' => 'integer', 'null' => true, 'default' => null),
+		'modified_user_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'comment' => 'æœªãƒ­ã‚°ã‚¤ãƒ³ã®äºº:0 ãƒ­ã‚°ã‚¤ãƒ³ã—ã¦ã‚‹äºº:users.id'),
 		'modified' => array('type' => 'datetime', 'null' => true, 'default' => null),
 		'indexes' => array(
-			'PRIMARY' => array('column' => 'id', 'unique' => 1),
-		),
-		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB'),
-	);
 
-/**
- * rss_readers_blocks table
- *
- * @author Kosuke Miura <k_miura@zenk.co.jp>
- * @var array
- */
-	public $rss_readers_blocks = array(
-		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
-		'block_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'unique'),
-		'created_user' => array('type' => 'integer', 'null' => true, 'default' => null),
-		'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
-		'modified_user' => array('type' => 'integer', 'null' => true, 'default' => null),
-		'modified' => array('type' => 'datetime', 'null' => true, 'default' => null),
-		'indexes' => array(
-			'PRIMARY' => array('column' => 'id', 'unique' => 1)
 		),
-		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB'),
-	);
-
-/**
- * rss_reader_settings table
- *
- * @author Kosuke Miura <k_miura@zenk.co.jp>
- * @var array
- */
-	public $rss_reader_settings = array(
-		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
-		'rss_readers_block_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'unique'),
-		'created_user' => array('type' => 'integer', 'null' => true, 'default' => null),
-		'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
-		'modified_user' => array('type' => 'integer', 'null' => true, 'default' => null),
-		'modified' => array('type' => 'datetime', 'null' => true, 'default' => null),
-		'indexes' => array(
-			'PRIMARY' => array('column' => 'id', 'unique' => 1)
-		),
-		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB'),
-	);
-
-/**
- * rss_reader_part_settings table
- *
- * @author Kosuke Miura <k_miura@zenk.co.jp>
- * @var array
- */
-	public $rss_reader_part_settings = array(
-		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
-		'rss_readers_block_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'unique'),
-		'part_id' => array('type' => 'integer', 'null' => false, 'default' => '0'),
-		'readable_content' => array('type' => 'boolean', 'null' => false, 'default' => false),
-		'editable_content' => array('type' => 'boolean', 'null' => false, 'default' => false),
-		'creatable_content' => array('type' => 'boolean', 'null' => false, 'default' => false),
-		'publishable_content' => array('type' => 'boolean', 'null' => false, 'default' => false),
-		'created_user' => array('type' => 'integer', 'null' => true, 'default' => null),
-		'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
-		'modified_user' => array('type' => 'integer', 'null' => true, 'default' => null),
-		'modified' => array('type' => 'datetime', 'null' => true, 'default' => null),
-		'indexes' => array(
-			'PRIMARY' => array('column' => 'id', 'unique' => 1)
-		),
-		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB'),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
 	);
 
 }
